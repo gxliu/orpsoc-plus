27a48bf7a2b2430f43b60113389aa4880647963b
isaac <ismapsa@gmail.com>
2013-11-11 03:08:46 -0300

    de0-nano pin mapping on top head JP1 for rs232 communication

---
+++ patches/orpsoc/boards/altera/de0_nano/syn/quartus/tcl/UART0_pin_assignments.tcl
@@ -0,0 +1,14 @@
+#set_location_assignment PIN_C16 -to uart0_srx_pad_i
+#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to uart0_srx_pad_i
+#set_location_assignment PIN_D15 -to uart0_stx_pad_o
+#set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to uart0_stx_pad_o
+
+#JP1
+
+#GPIO_00
+set_location_assignment PIN_D3 -to   uart0_srx_pad_i 
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to uart0_srx_pad_i 
+
+#GPIO_01
+set_location_assignment PIN_C3 -to uart0_stx_pad_o
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to uart0_stx_pad_o
